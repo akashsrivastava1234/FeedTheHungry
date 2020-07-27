da3848ac2618f1de0034572a308e77dac66d027c
Akash Srivastava <aksri@microsoft.com>
2020-07-27 17:30:08 +0530

    First Commit

---
+++ src/main/java/com/microsoft/feedthehungry/FeedTheHungryServerApplication.java
@@ -0,0 +1,13 @@
+package com.microsoft.feedthehungry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedTheHungryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedTheHungryServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microsoft/feedthehungry/FeedTheHungryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microsoft.feedthehungry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedTheHungryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
